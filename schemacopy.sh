f21110914a48991059e89960bf2c460fc29c2868
rvnataraj <rv.nataraj@gmail.com>
2020-10-07 15:59:04 +0530

    schcopy

--- schemacopy.sh
+++ schemacopy.sh
@@ -20,7 +20,9 @@ then
                 dc[$i]="$line"
                 i=$((i+1))
         done <$file
-
+	mydumper --host=${sc[1]} --port=${sc[2]} --user=${sc[3]} --password=${sc[4]} --outputdir=tmpsch --no-data
+	myloader  --host=${dc[1]} --port=${dc[2]} --user=${dc[3]} --password=${dc[4]} --directory=tmpsch --overwrite-tables
+	rm -r tmpsch
 	echo "Database Operations...."
 	file=$3
         i=1;
@@ -28,31 +30,11 @@ then
                 lengt=${#line}
                 echo "$line - $lengt"
                 tn[$i]="$line"
-                i=$((i+1))
-
-		tbarr=($line)
-		echo ${tbarr[0]}
-		mydumper --host=${sc[1]} --port=${sc[2]} --user=${sc[3]} --password=${sc[4]} --outputdir=tmp --tables-list=${tbarr[0]}
-		echo ${tbarr[1]}
-		if [ ${tbarr[1]} == "1" ]; then
-			echo "include schema"
-		else
-			echo "deleting schema file"
-			rm tmp/${tbarr[0]}-schema.sql
-		fi
-		if [ ${tbarr[2]} == "1" ]; then
-			echo "include table data"
-                else
-                        echo "deleting table data"
-			rm tmp/${tbarr[0]}.sql
-                fi
+		echo ${tn[i]}
+		mydumper --host=${sc[1]} --port=${sc[2]} --user=${sc[3]} --password=${sc[4]} --outputdir=tmp --tables-list=${tn[i]}
 		myloader  --host=${dc[1]} --port=${dc[2]} --user=${dc[3]} --password=${dc[4]} --directory=tmp --overwrite-tables
-		rm tmp/*.sql
-	        for j in "${tbarr[@]}"
-        	do
-
-                	echo "$j"
-        	done
+		#rm tmp/*.sql
+		i=$((i+1))
         done <$file
 else
 	echo "<source db> <destination db> <dbtables.config>"
